43d9c6506d152660f383da5adad598f701998f07
FeSette <felipe_sette@outlook.com>
2025-07-11 10:22:46 -0300

    Adicionando IA e finalizando o projeto

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ const markdownToHTML = (text) => {
     return converter.makeHtml(text)
 }
 
-//AIzaSyDDwlzDnnNfRYsJ-_KUTuO6-HvUr9gboTE
-
 const questionAI = async (question, game, apiKey) => {
     const model = 'gemini-2.5-flash'
     const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
